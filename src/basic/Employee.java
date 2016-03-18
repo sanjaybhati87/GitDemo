cdf38a7a5a554c0af6a81d51836a91fa26cde3ae
sanjaybhati87 <sanjaythakur287@gmail.com>
2016-03-18 10:49:13 +0530

    First commit for GitDemo
    First commit for GitDemo

---
+++ src/basic/Employee.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final String name;
+		final int age;
+		final int empId;
+		
+		
+
+	}
+
+}
